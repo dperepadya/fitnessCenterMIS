0790781cdddeae6d4befbd5200d0b2a7bafd451a
Dmitry Perepadya <dperepadya@gmail.com>
2024-06-14 16:31:30 +0000

    14Jun2024SQLAlchemy

--- login/handlers.py
+++ login/handlers.py
@@ -21,20 +21,7 @@ def get_user_from_db(username, password):
     where_cond = {'credentials.login': username, 'credentials.password': password}
     query = qg.get_select_sql_join_query(table, select_params, join_tables, join_params, where_cond)
     print(query)
-    '''
-    query = ("SELECT clients.id AS client_id, name AS client_name FROM credentials"
-            " JOIN clients ON credentials.client_id = clients.id"
-            " WHERE credentials.login = 'larry123' AND credentials.password = '12345'")
-    query = 'select name from clients where id=1'
-    conn = sqlite3.connect('fitnessdb.db')
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    cursor.execute(query)
-    print(query)
-    res = cursor.fetchone()
-    #conn.close()
-    return res
-    '''
+
     with SQLLiteDatabase('fitnessdb.db') as db:
         user = db.fetch(query, False)
     if user:
